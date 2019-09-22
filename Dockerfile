b4fbe65ade733f0cb0cc62c0ac9fc7a4a8b95192
Lois Di Qual <lois@takescoop.com>
2019-09-22 02:48:22 -0700

    Getting somewhere

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,79 @@
-FROM scottyhardy/docker-wine
+FROM ubuntu:bionic
 
-RUN apt-get update && apt-get -y install xvfb
+# Install package dependencies
+RUN export DEBIAN_FRONTEND="noninteractive" \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends \
+        apt-transport-https \
+        ca-certificates \
+        gosu \
+        p7zip \
+        unzip \
+        wget \
+        winbind \
+        software-properties-common \
+        xvfb \
+        gpg-agent \
+        cabextract
+        
+ENV WINE_VERSION 4.16
 
+# Install wine
+RUN wget https://dl.winehq.org/wine-builds/winehq.key \
+    && apt-key add winehq.key \
+    && apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main" \
+    && add-apt-repository ppa:cybermax-dexter/sdl2-backport \
+    && dpkg --add-architecture i386 \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends winehq-devel=${WINE_VERSION}~bionic \
+    && rm winehq.key
+
+# Create wineuser
+RUN groupadd -g 1010 wineuser \
+    && useradd --shell /bin/bash --uid 1010 --gid 1010 --create-home --home-dir /home/wineuser wineuser \
+    && chown -R wineuser:wineuser /home/wineuser
+
+# Install entrypoint
 COPY entrypoint.sh /usr/bin/entrypoint
 RUN chmod +x /usr/bin/entrypoint
 
-RUN xvfb-run --auto-servernum winetricks -q dotnet452
-ADD --chown=wineuser:wineuser https://aka.ms/vs/15/release/vs_buildtools.exe /home/wineuser/vs_BuildTools.exe
+# Install winetricks
+ADD --chown=wineuser:wineuser https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks /usr/bin/winetricks
+RUN chmod +x /usr/bin/winetricks
 
-RUN xvfb-run --auto-servernum wine /home/wineuser/vs_BuildTools.exe --passive --layout c:\BT2017offline --add Microsoft.VisualStudio.Workload.VCTools
+# Install node
+RUN wget -O- https://deb.nodesource.com/setup_10.x | bash
+RUN apt-get install -y nodejs
+
+ENV ADDONS_C_URL https://raw.githubusercontent.com/wine-mirror/wine/wine-${WINE_VERSION}/dlls/appwiz.cpl/addons.c
+
+# Install wine-mono
+RUN mkdir -p /usr/share/wine/mono
+RUN MONO_VERSION=$(wget -O- ${ADDONS_C_URL} | grep '#define MONO_VERSION' | sed -n 's/.*"\(.*\)"/\1/p') \
+    && wget https://dl.winehq.org/wine/wine-mono/${MONO_VERSION}/wine-mono-${MONO_VERSION}.msi -O /usr/share/wine/mono/wine-mono-${MONO_VERSION}.msi \
+    && chown wineuser:wineuser /usr/share/wine/mono/wine-mono-${MONO_VERSION}.msi
+
+# Install wine-gecko
+RUN mkdir -p /usr/share/wine/gecko
+RUN GECKO_VERSION=$(wget -O- ${ADDONS_C_URL} | grep '#define GECKO_VERSION' | sed -n 's/.*"\(.*\)"/\1/p') \
+    && wget https://dl.winehq.org/wine/wine-gecko/${GECKO_VERSION}/wine_gecko-${GECKO_VERSION}-x86.msi -O /usr/share/wine/gecko/wine_gecko-${GECKO_VERSION}-x86.msi \
+    && wget https://dl.winehq.org/wine/wine-gecko/${GECKO_VERSION}/wine_gecko-${GECKO_VERSION}-x86_64.msi -O /usr/share/wine/gecko/wine_gecko-${GECKO_VERSION}-x86_64.msi \
+    && chown wineuser:wineuser /usr/share/wine/gecko/wine_gecko-${GECKO_VERSION}-x86.msi \
+    && chown wineuser:wineuser /usr/share/wine/gecko/wine_gecko-${GECKO_VERSION}-x86_64.msi
+
+# Now we're wineuser
+USER wineuser:wineuser
+
+RUN WINEARCH=win32 xvfb-run --auto-servernum wine wineboot --init \
+    && xvfb-run --auto-servernum winetricks -q dotnet461 cmd
+
+# Create C:\BuildTools
+RUN mkdir /home/wineuser/.wine/drive_c/BuildTools
+
+# Install build tools
+ADD deps/ /home/wineuser/
+WORKDIR /home/wineuser/deps
+RUN npm install \
+    && node index.js /home/wineuser/.wine/drive_c/BuildTools
 
 ENTRYPOINT ["/usr/bin/entrypoint"]
---
+++ deps/index.js
@@ -0,0 +1,176 @@
+const _ = require('lodash')
+const fs = require('fs-extra')
+const Promise = require('bluebird')
+const DepGraph = require('dependency-graph').DepGraph
+const path = require('path')
+const tmp = require('tmp-promise')
+const decompress = require('decompress')
+const download = require('download')
+const fetch = require('node-fetch')
+const execa = require('execa')
+
+async function downloadFile({ src, dst }) {
+    console.log(`Downloading ${src}`)
+    const downloadPromise = download(src)
+    downloadPromise.pipe(fs.createWriteStream(dst))
+    await downloadPromise
+}
+
+async function runCommand(path, args, options) {
+    console.log(`Running ${path} ${args.join(' ')}`)
+    const promise = execa(path, args, options)
+    promise.stdout.pipe(process.stdout)
+    promise.stderr.pipe(process.stderr)
+    return promise
+}
+
+function findPackageAndDependencies({ json, id, chip }) {
+    
+    const packages = _.filter(json.packages, pkg => {
+                
+        const hasSameId = pkg.id.toLowerCase() === id.toLowerCase()
+        if (!hasSameId) {
+            return false
+        }
+        
+        if (chip) {
+            const hasSameChip = pkg.chip.toLowerCase() === chip.toLowerCase()
+            const isChipNeutral = pkg.chip.toLowerCase() === 'neutral'
+            return hasSameChip || isChipNeutral
+        }
+        
+        return true
+    })
+    
+    const ignoredDependencies = [
+        'Microsoft.Net.4.6.1.FullRedist.Resources',
+        'Microsoft.Net.4.6.1.SDK.Resources'
+    ]
+    
+    const dependencies = _.flatten(_.map(packages, pkg => {
+        const filteredDependencies = _.pickBy(_.omit(pkg.dependencies, ignoredDependencies), dep => {
+            if (!dep.type) {
+                return true
+            }
+            return dep.type.toLowerCase() !== 'optional' && dep.type.toLowerCase() !== 'recommended'
+        })
+        return _.flatten(_.map(filteredDependencies, (dep, depId) => {
+            const chip = _.isObject(dep) ? dep.chip : null
+            return findPackageAndDependencies({ json, id: depId, chip: chip })
+        }))
+    }))
+    
+    return [...packages, ...dependencies]
+}
+
+async function installVsix({ pkg, dst }) {
+    
+    const { path: zipPath } = await tmp.file()
+    const url = pkg.payloads[0].url
+    
+    console.log(`Downloading ${pkg.id}`)
+    const zipPath = await downloadFile({ src: url, dst: zipPath })
+    await Promise.delay(0.5)
+    
+    console.log(`Decompressing ${pkg.id}`)
+    await decompress(zipPath, dst, {
+        filter: file => _.startsWith(file.path, 'Contents/'),
+        map: file => {
+            file.path = file.path.split('Contents/')[1].replace('%20', ' ')
+            return file
+        }
+    })
+}
+
+async function installMsi({ pkg, dst }) {
+    
+    console.log(`Downloading ${pkg.id}`)
+    const { path: downloadDir } = await tmp.dir()
+    for (const payload of pkg.payloads) {
+        await downloadFile({
+            src: payload.url,
+            dst: path.join(downloadDir, payload.fileName)
+        })
+    }
+    
+    console.log(`Installing ${pkg.id}`)
+    const msiPayload = _.find(pkg.payloads, payload => _.endsWith(payload.fileName, 'msi'))
+    const msiProperties = _.map(pkg.msiProperties || {}, (value, key) => `${key}=${value}`)
+    await runCommand('msiexec', [
+        '/i', path.join(downloadDir, msiPayload.fileName),
+        ...msiProperties,
+        '/qn',
+    ])
+}
+
+async function installPackage({ pkg, dst }) {
+    switch (pkg.type.toLowerCase()) {
+    case 'vsix':
+        // return installVsix({ pkg, dst })
+        break
+    case 'exe':
+        console.log(pkg.id, pkg.payloads.map(p => p.url), 'exe')
+        break;
+    case 'msi':
+        return installMsi({ pkg, dst })
+        break;
+    }
+}
+
+async function run() {
+    
+    let extractPath = process.argv.length > 2 ? path.resolve(_.last(process.argv)) : null
+    if (extractPath === null) {
+        const { path: tmpDir } = await tmp.dir()
+        extractPath = tmpDir
+    }
+    console.log('Extraction path', extractPath)
+    
+    // Download manifest
+    const manifestUrl = 'https://aka.ms/vs/15/release/channel'
+    const manifest = await (await fetch(manifestUrl)).json()
+    
+    // Extract catalog url from manifest and download it
+    const catalog = _.find(manifest.channelItems, item => {
+        return item.type.toLowerCase() === 'manifest' && item.id === 'Microsoft.VisualStudio.Manifests.VisualStudio'
+    })
+    const catalogUrl = catalog.payloads[0].url
+    const catalogJson = await (await fetch(catalogUrl)).json()
+    
+    // Only look for english or neutral packages
+    const onlyEnglish = {
+        ...catalogJson,
+        packages: catalogJson.packages.filter(pkg => {
+            return _.isUndefined(pkg.language) || pkg.language === 'en-US' || pkg.language === 'neutral'
+        })
+    }
+    
+    // Add VCTools
+    const vcToolsPackages = findPackageAndDependencies({
+        json: onlyEnglish,
+        id: 'Microsoft.VisualStudio.Workload.VCTools'
+    })
+    
+    // Add BuildTools
+    const buildToolsPackages = findPackageAndDependencies({
+        json: onlyEnglish,
+        id: 'Microsoft.VisualStudio.Product.BuildTools'
+    })
+    
+    // Remove duplicates
+    const uniquePackages = _.uniqBy([...vcToolsPackages, ...buildToolsPackages], pkg => {
+        const chip = pkg.chip || 'neutral'
+        return `${pkg.id}-${chip}`
+    })
+    
+    console.log(uniquePackages.map(pkg => pkg.id))
+    
+    // Install all pages
+    for (pkg of uniquePackages) {
+        await installPackage({ pkg, dst: extractPath })
+    }
+}
+
+run().catch(err => {
+    console.error(err.stack)
+})
